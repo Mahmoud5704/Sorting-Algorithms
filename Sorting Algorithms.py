2ffed2fa1241206b39f621eac461ab65ad073fd5
Mazen Elkashlan <elkashlanmazen66@gmail.com>
2026-03-13 17:28:55 +0200

    Updated File

--- Sorting Algorithms.py
+++ Sorting Algorithms.py
@@ -125,34 +125,35 @@ def kth_smallest(arr, k):
 
 ################################################# HEAP SORT ##############################################################    
 
-def build_heap(A,n):
-    n = len(A)
-    for i in range(n//2,0,-1):  
-        heapify(A,n,i)
+def build_heap(A, n):
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(A, n, i)
 
-def heapify(A,n,i):
-    largest=i
-    left=2*i
-    right=2*i+1
-    if left<n and A[left]>A[largest]:
-        largest=left
-    if right<n and A[right]>A[largest]:
-        largest=right
-    if largest !=i:
-        A[i],A[largest]=A[largest],A[i]
-        heapify(A,n,largest)
+def heapify(A, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and A[left] > A[largest]:
+        largest = left
+    if right < n and A[right] > A[largest]:
+        largest = right
+    if largest != i:
+        A[i], A[largest] = A[largest], A[i]
+        heapify(A, n, largest)
 
 def heap_sort(A):
-    n=len(A)-1 #start from index 1
-    build_heap(A,n)
-    for i in range (n,1,-1):
-        A[1],A[i]=A[i],A[1]
-        heapify(A,i-1,1)
+    n = len(A)
+    build_heap(A, n)
+    for i in range(n - 1, 0, -1):
+        A[0], A[i] = A[i], A[0]
+        heapify(A, i, 0)
 
 ###############################################################################################################    
 
 def main():
-    arr = [0, 4, 10, 3, 5, 1]
+    arr = [7, 10, 4, 3, 20, 15]
+    print(kth_smallest(arr, 1))
     #merge_sort(arr)
     #hyprid_sort(arr,3)
     #quickSort(arr,0,8)
